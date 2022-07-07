56f30fbfa315c9e392877f33a0610e85df32f616
LouiseWJ <wanju412@gmail.com>
2022-07-07 22:28:57 +0800

    infra: create environments and structure of projects

---
+++ src/main.py
@@ -0,0 +1,29 @@
+from functions.create_life_values_table import create_life_values_table
+
+
+def find_life_values():
+    print('Hello, I am coming to help you find your life values. \nPlease select the 10 most important life values based on the table below: \n')
+    life_values_table = create_life_values_table()
+    
+    print('Type your 10 life values by this format: value1, value2, value3, ..., value10')
+    ten_life_values = input('Your 10 most important life values: ')
+    #check_ten_life_values(ten_life_values)
+    
+    print('''
+        Based on the 10 life values, you will be asked to select the most important values
+        from two values randomly choosed. There are two rounds of the process.
+    '''
+    )
+    raw_result = compare_two_values()
+    
+    sort_result = _sort_values()
+    sort_result = resort_values_if_same_order()
+    final result = create_report()
+    print('it is your result')
+    
+    
+    
+    #print('the similarity of the two rounds. below is the same values')
+
+if __name__ == '__main__':
+    find_life_values()
