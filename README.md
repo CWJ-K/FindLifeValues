b04bdf2b8054b40848ba85e96a00fec075d8b5dc
LouiseWJ <wanju412@gmail.com>
2022-07-08 11:18:07 +0800

    refactor(create_report, resort_values): add writer to create and check report

--- README.md
+++ README.md
@@ -7,3 +7,5 @@ the tool to help order your lifeview
 1. add timeout: 3 seconds
 2. become more elastic, can use other values 
 3. add break option => users can restart specific question
+4. change the font of words
+5. add the check of life values input
--- src/main.py
+++ src/main.py
@@ -1,30 +1,47 @@
+import time
+
 from functions.create_life_values_table import create_life_values_table
 from functions.ask_the_importance_of_values import ask_the_importance_of_values
-from functions.create_report import create_report
-from functions.resort_values_if_same_grades import resort_values_if_same_grades
+from functions.writer import Writer
+
 
 def find_life_values():
     
-    
-    print('Hello, I am coming to help you find your life values. \nPlease select the 10 most important life values based on the table below: \n')
-    life_values_table = create_life_values_table()
-    
-    print('Type your 10 life values by this format: value1, value2, value3, ..., value10')
-    ten_life_values = input('Your 10 most important life values: ')
-    #check_ten_life_values(ten_life_values)
-
-    print('''Based on the 10 life values, you will be asked to select the most important values 
-    from two values randomly chosen. There are two rounds of the process.
-    '''
+    print(
+        'Hello, I am coming to help you find your life values.\n',
+        'The whole process will be conducted in two rounds.\n', 
+        'Please select the 10 most important life values based on the table below: \n',
     )
-    answers = ask_the_importance_of_values(ten_life_values)
     
-    sort_report = create_report(answers)
-    sort_report = resort_values_if_same_grades(sort_report)
+    time.sleep(2)
+
+    create_life_values_table()
     
-    print('it is your result \n', sort_report)
+    time.sleep(2)
     
+    print('Type in your 10 life values in the format of: value1, value2, value3, ..., value10')
+    ten_life_values = input('Your 10 most important life values are: ')
+
+    time.sleep(2)
     
+    print(
+        'Based on your 10 life values,\n',
+        'you are going to compare the importance of two values randomly chosen.',
+    )
+
+    time.sleep(2)
+
+    print('Start to compare them!')
+
+    time.sleep(1)
+
+    responses = ask_the_importance_of_values(ten_life_values)
+    
+    writer = Writer()
+    writer.generate_report(responses)
+    writer.rerank_report_if_duplicated_ranks()
+    
+    print('This is your result: \n', writer.report)
     
     #print('the similarity of the two rounds. below is the same values')
 
